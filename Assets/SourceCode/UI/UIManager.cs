7d12dd08886946de5d958fbd78b26e5b7f8a9a39
wolf <aminulhussain27@gmail.com>
2019-05-13 01:20:15 +0530

    UIManager added

---
+++ Assets/SourceCode/UI/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void ExitButtonAction()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+	
+
+}
